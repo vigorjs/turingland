89a75a2aeae2729432a97969fdda383965488cfe
Fauzan Zaman <63907675+fauzangan@users.noreply.github.com>
2025-01-14 19:49:34 +0700

    Add Edit Feat

--- app/Repositories/PropertyImage/PropertyImageRepository.php
+++ app/Repositories/PropertyImage/PropertyImageRepository.php
@@ -8,4 +8,7 @@ interface PropertyImageRepository extends Repository{
 
     // Write something awesome :)
     public function createImage($images);
+    public function getImagesByPropertyId($propertyId);
+    public function updateImage($id, array $data);
+    public function deleteImage($id);
 }
--- app/Services/PropertyImage/PropertyImageServiceImplement.php
+++ app/Services/PropertyImage/PropertyImageServiceImplement.php
@@ -5,34 +5,48 @@ namespace App\Services\PropertyImage;
 use LaravelEasyRepository\ServiceApi;
 use App\Repositories\PropertyImage\PropertyImageRepository;
 
-class PropertyImageServiceImplement extends ServiceApi implements PropertyImageService{
+class PropertyImageServiceImplement extends ServiceApi implements PropertyImageService
+{
 
-    /**
-     * set title message api for CRUD
-     * @param string $title
-     */
-     protected string $title = "";
-     /**
-     * uncomment this to override the default message
-     * protected string $create_message = "";
-     * protected string $update_message = "";
-     * protected string $delete_message = "";
-     */
+  /**
+   * set title message api for CRUD
+   * @param string $title
+   */
+  protected string $title = "";
+  /**
+   * uncomment this to override the default message
+   * protected string $create_message = "";
+   * protected string $update_message = "";
+   * protected string $delete_message = "";
+   */
 
-     /**
-     * don't change $this->mainRepository variable name
-     * because used in extends service class
-     */
-     protected PropertyImageRepository $mainRepository;
+  /**
+   * don't change $this->mainRepository variable name
+   * because used in extends service class
+   */
+  protected PropertyImageRepository $mainRepository;
 
-    public function __construct(PropertyImageRepository $mainRepository)
-    {
-      $this->mainRepository = $mainRepository;
-    }
+  public function __construct(PropertyImageRepository $mainRepository)
+  {
+    $this->mainRepository = $mainRepository;
+  }
 
-    // Define your custom methods :)
-    public function createImage($image)
-    {
-      return $this->mainRepository->createImage($image);
-    }
+  // Define your custom methods :)
+  public function createImage($image)
+  {
+    return $this->mainRepository->createImage($image);
+  }
+
+  public function getImagesByPropertyId($propertyId) 
+  {
+    return $this->mainRepository->getImagesByPropertyId($propertyId);
+  }
+
+  public function updateImage($id, array $data) 
+  {
+    return $this->mainRepository->updateImage($id, $data);
+  }
+  public function deleteImage($id) {
+    return $this->mainRepository->deleteImage($id);
+  }
 }
