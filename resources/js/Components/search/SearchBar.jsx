89f5ee432e092feb6f533ef9a2730d82a184ea0a
AlanPratama <pralan76@gmail.com>
2025-01-24 10:04:32 +0700

    fix placeholder search

--- resources/js/Components/search/SearchBar.jsx
+++ resources/js/Components/search/SearchBar.jsx
@@ -105,7 +105,7 @@ const SearchBar = ({ categories, areas, filters, setPropertiesData }) => {
                         // onChange={(e) => setData("title", e.target.value)}
                         onChange={(e) => debounce(e.target.value)}
                         type="text"
-                        placeholder="Cari properti, lokasi, area, developer, dan agent"
+                        placeholder="Cari properti, lokasi, area, developer, atau agent"
                         className="flex-1 px-4 py-2 border border-[#C6C6C6] bg-[#EDEDED] rounded-lg w-2/3"
                     />
                     {/* <Button
